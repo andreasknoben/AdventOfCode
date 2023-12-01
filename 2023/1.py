9305b35ce54e631c7393d44b016fd75171d1b64e
Andreas Knoben <mail@andreasknoben.nl>
2023-12-01 17:19:59 +0100

    Solution day 1 part 1 and 2

--- 2023/1.py
+++ 2023/1.py
@@ -3,11 +3,38 @@ import re
 f = open('./1.txt', 'r')
 lines = f.readlines()
 
-total = 0
+def part1():
+    total = 0
+    for line in lines:
+        first = re.search(r'[0-9]+', line).group()[0]
+        last = re.search(r'[0-9]+', line[::-1]).group()[0]
+        total += int(first + last)
 
-for line in lines:
-    first = re.search(r'[0-9]+', line).group()[0]
-    last = re.search(r'[0-9]+', line[::-1]).group()[0]
-    total += int(first + last)
+    return total
 
-print(total)
+
+def part2():
+    regex_first = r'([0-9]|one|two|three|four|five|six|seven|eight|nine){1}'
+    regex_last = r'([0-9]|one|two|three|four|five|six|seven|eight|nine)?$'
+
+    total = 0
+    numbers = {'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6', 'seven': '7', 'eight': '8', 'nine': '9'}
+
+    for line in lines:
+        first = re.search(regex_first, line).group()
+        last = re.search(regex_last, line).group()
+        while last == '' or last == None:
+            line = line[:-1]
+            last = re.search(regex_last, line).group()
+
+        if first in numbers.keys():
+            first = numbers[first]
+        if last in numbers.keys():
+            last = numbers[last]
+
+        total += int(first + last)
+
+    return total
+
+print(part1())
+print(part2())
