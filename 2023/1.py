54cecaa007732f3295c3c176642572e8a1759185
Andreas Knoben <mail@andreasknoben.nl>
2023-12-01 16:51:13 +0100

    Slightly improved solution day 1 2023

--- 2023/1.py
+++ 2023/1.py
@@ -3,13 +3,11 @@ import re
 f = open('./1.txt', 'r')
 lines = f.readlines()
 
-numbers = []
+total = 0
 
 for line in lines:
     first = re.search(r'[0-9]+', line).group()[0]
     last = re.search(r'[0-9]+', line[::-1]).group()[0]
-    
-    num = first + last
-    numbers.append(int(num))
+    total += int(first + last)
 
-print(sum(numbers))
+print(total)
