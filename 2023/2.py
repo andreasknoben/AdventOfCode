a981a6483b0d380ea6fde9afe2fd5f64110e3f99
Andreas Knoben <mail@andreasknoben.nl>
2023-12-03 21:33:01 +0100

    Solution day 2 part 2

--- 2023/2.py
+++ 2023/2.py
@@ -31,4 +31,28 @@ def part1():
     possible_id = [games[i] for i in range(len(games)) if possible[i]]
     return sum(possible_id)
 
+
+def part2():
+    games = []
+    powers = []
+
+    for i in range(len(lines)):
+        splitline = lines[i].split(':')
+        games.append(int(splitline[0].split(' ')[1]))
+        sets = splitline[1].split(';')
+
+        largest = {'red': 0, 'green': 0, 'blue': 0}
+        for s in sets:
+            s = s.split(',')
+            for item in s:
+                item = item.strip()
+                res = item.split(' ')
+                if int(res[0]) > largest[res[1]]:
+                    largest[res[1]] = int(res[0])
+
+        powers.append(largest['red'] * largest['green'] * largest['blue'])
+
+    return sum(powers)
+
 print(part1())
+print(part2())
